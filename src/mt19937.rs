e3c252aa6c44ef7f72696ef01e88e8a77b26ea75
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2022-05-22 14:57:52 +0200

    Clean up mt19937

--- src/mt19937.rs
+++ src/mt19937.rs
@@ -1,5 +1,5 @@
 const N: usize = 624;
-const M: usize = 397;
+const LOWER_MASK: u32 = 0x7fffffff;
 
 pub struct MT19937 {
   v: [u32; N],
@@ -8,12 +8,13 @@ pub struct MT19937 {
 
 impl MT19937 {
   pub fn from_seed(seed: u32) -> Self {
-    let mut mt19937 = Self {
-      v: [0; N],
-      i: usize::MAX,
-    };
-    mt19937.reseed(seed);
-    mt19937
+    let mut v = [0; N];
+    v[0] = seed;
+    for i in 1..N {
+      let x = v[i-1];
+      v[i] = 1812433253 * (x ^ (x >> 30)) + i as u32;
+    }
+    Self { v, i: N }
   }
 
   pub fn gen(&mut self) -> u32 {
@@ -29,21 +30,11 @@ impl MT19937 {
     y
   }
 
-  pub fn reseed(&mut self, seed: u32) {
-    self.i = N;
-    self.v[0] = seed;
-    for i in 1..N {
-      let x = self.v[i-1];
-      self.v[i] = 1812433253 * (x ^ (x >> 30)) + i as u32;
-    }
-  }
-
   fn twist(&mut self) {
-    const LOWER_MASK: u32 = 0x7fffffff;
     for i in 0..N {
-      let x = (self.v[i] & !LOWER_MASK) | (self.v[(i+1) % N] & LOWER_MASK);
+      let x = (self.v[i] & !LOWER_MASK) | (self.v[(i + 1) % N] & LOWER_MASK);
       let t = if (x & 1) == 1 {0x9908b0df} else {0};
-      self.v[i] = self.v[(i+M) % N] ^ (x >> 1) ^ t;
+      self.v[i] = self.v[(i + 397) % N] ^ (x >> 1) ^ t;
     }
     self.i = 0;
   }
@@ -55,6 +46,7 @@ mod tests {
 
   #[test]
   fn test_cpp_standard() {
+    // testcase from the c++ standard
     let mut mt19937 = MT19937::from_seed(5489);
     for _ in 0..9999 { mt19937.gen(); }
     assert_eq!(mt19937.gen(), 4123659995);
@@ -76,6 +68,8 @@ mod tests {
       2678185683,
     ];
     let mut mt19937 = MT19937::from_seed(0);
-    for x in EXPECTED { assert_eq!(mt19937.gen(), x) }
+    for x in EXPECTED {
+      assert_eq!(mt19937.gen(), x);
+    }
   }
 }
