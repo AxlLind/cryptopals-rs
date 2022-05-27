5b5238a24d00f292f34332660364ce0dce726913
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2022-05-27 22:50:03 +0200

    Clean up 31

--- web_timing/src/bin/31.rs
+++ web_timing/src/bin/31.rs
@@ -3,39 +3,28 @@ use std::time::{Duration, Instant};
 use reqwest::{self, StatusCode};
 use rayon::prelude::*;
 
-fn to_hex_str(bytes: &[u8]) -> String {
-  bytes.iter()
-    .flat_map(|&b| [b >> 4, b & 0b1111])
-    .map(|b| match b {
-      0..=9 => (b + b'0') as char,
-      10..=16 => (b + b'a' - 10) as char,
-      _ => unreachable!(),
-    })
-    .collect()
-}
-
 fn build_and_spawn_server() -> Child {
   println!("Building server...");
   let build_status = Command::new("cargo")
     .args(["build", "--quiet", "--release", "-p", "web_timing"])
     .status()
-    .expect("failed to build web_timing");
+    .expect("failed to build server");
   assert!(build_status.success());
 
-  println!("Starting server...");
+  println!("Launching server...");
   Command::new("cargo")
     .args(["run", "--quiet", "--release", "-p", "web_timing", "--", "50"])
     .stdout(Stdio::null())
     .spawn()
-    .expect("failed to build web_timing")
+    .expect("failed to launch server")
 }
 
 fn find_byte(mac: [u8; 20], i: usize) -> u8 {
   (0..=0xff).into_par_iter().max_by_key(|&b| {
     let mut mac = mac;
     mac[i] = b;
-    let signature = to_hex_str(&mac);
-    let url = format!("http://localhost:9000/test?file=a.txt&signature={}", signature);
+    let signature = cryptopals_rs::to_hex_str(&mac);
+    let url = format!("http://localhost:9000/test?file=secret.txt&signature={}", signature);
     let now = Instant::now();
     reqwest::blocking::get(url).unwrap();
     now.elapsed().as_micros()
@@ -43,6 +32,7 @@ fn find_byte(mac: [u8; 20], i: usize) -> u8 {
 }
 
 fn main() {
+  // warning: this takes several minutes to run
   let mut server_process = build_and_spawn_server();
   std::thread::sleep(Duration::from_secs(2)); // wait for server to boot
 
@@ -50,11 +40,13 @@ fn main() {
   let mut signature = [0; 20];
   for i in 0..20 {
     signature[i] = find_byte(signature, i);
-    println!("{:x}", signature[i]);
+    println!("{}={:02x}", i, signature[i]);
   }
-  println!("{}", to_hex_str(&signature));
 
-  let url = format!("http://localhost:9000/test?file=a.txt&signature={}", to_hex_str(&signature));
-  assert_eq!(reqwest::blocking::get(url).unwrap().status(), StatusCode::OK);
+  let url = format!("http://localhost:9000/test?file=secret.txt&signature={}", cryptopals_rs::to_hex_str(&signature));
+  let res = reqwest::blocking::get(url).unwrap();
+  assert_eq!(res.status(), StatusCode::OK);
+  println!("Found signature: {}", cryptopals_rs::to_hex_str(&signature));
+  println!("Secret: {:?}", String::from_utf8(res.bytes().unwrap().to_vec()).unwrap());
   server_process.kill().unwrap();
 }
